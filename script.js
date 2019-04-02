f976043738bdcd9147a3b5cc7b57f8a3e39524b0
Austin <fascinatedbydinosaurs@gmail.com>
2019-04-01 19:11:28 -0500

    A9

---
+++ script.js
@@ -0,0 +1,22 @@
+
+new Vue({
+  el: "#myapp",
+  data: {
+    llamasAreAwesome:true,
+    todos: [
+    { text: "llama poop has almost no odor." },
+    { text: "llamas are weird." },
+    { text: "llamas are not very nice." },
+    { text: "llamas think they're better than us." },
+    { text: "llamas talk with their mouths full." },
+    { text: "llamas think Star Wars is dumb." },
+    { text: "llamas have bad breath." },
+    { text: "llamas play loud music when you're trying to sleep." },
+    { text: "llamas don't know how to read." },
+    { text: "llamas tell bad jokes." },
+
+    ]
+
+  }
+
+});
